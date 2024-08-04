9641813bb3612647b6cfa855cf9388a378076793
geroarmani <gerogrigorov@gmail.com>
2024-08-04 21:19:24 +0200

    Setting up the backend folder

---
+++ backend/models/user.js
@@ -0,0 +1,7 @@
+const pool = require("../db")
+
+const createUser = async (username, email, password) => {
+  const result = await pool.query(
+    "INSTERT INTO users ()"
+  )
+}
---
+++ backend/routes/authentication.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/register", async (req, res) => {
+
+})
